c85dd721e7a325169190c084a43de38f367da495
Pingu Carsti <ehbrecht@dkrz.de>
2018-10-22 13:42:49 +0200

    updated tests

--- tests/test_wps_caps.py
+++ tests/test_wps_caps.py
@@ -13,6 +13,4 @@ def test_wps_caps():
                             '/wps:Process'
                             '/ows:Identifier')
     assert sorted(names.split()) == [
-        'inout',
-        'sleep',
-        'wordcounter']
+        'sleep']
