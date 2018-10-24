dafaa3d22be0133b896b4d9bd41d6c4cff82d78b
Pingu Carsti <ehbrecht@dkrz.de>
2018-10-24 17:27:03 +0200

    configured climaf log dir

--- c4cds/search.py
+++ c4cds/search.py
@@ -7,7 +7,7 @@ LOGGER = logging.getLogger('PYWPS')
 
 # init climaf
 os.environ['CLIMAF_CACHE'] = os.path.join(tempfile.gettempdir(), 'climaf_cache')
-# os.environ['CLIMAF_LOG_DIR'] = tempfile.gettempdir()
+os.environ['CLIMAF_LOG_DIR'] = tempfile.gettempdir()
 
 from climaf.api import ds
 from climaf.dataloc import dataloc
