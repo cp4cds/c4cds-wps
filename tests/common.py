cacb14126d049ea630ca0426395b5c360898f3c8
Pingu Carsti <ehbrecht@dkrz.de>
2018-10-25 13:36:40 +0200

    fixed conda env#

--- tests/common.py
+++ tests/common.py
@@ -3,7 +3,7 @@ from pywps.tests import WpsClient, WpsTestResponse
 ARCHIVE_BASE = '/opt/data'
 
 CMIP5_NC = "/opt/data/cmip5/output1/MOHC/HadGEM2-ES/historical/day/atmos/day/r1i1p1/v20120716/tas/tas_day_HadGEM2-ES_historical_r1i1p1_19791201-19891130.nc"  # noqa
-CORDEX_NC = "/opt/data/cordex/tasmin_AFR-44i_ECMWF-ERAINT_evaluation_r1i1p1_MOHC-HadRM3P_v1_mon_199001-199012.nc"
+CORDEX_NC = "/opt/data/cordex/output/AFR-44i/MOHC/ECMWF-ERAINT/evaluation/r1i1p1/MOHC-HadRM3P/v1/mon/tasmin/v20131211/tasmin_AFR-44i_ECMWF-ERAINT_evaluation_r1i1p1_MOHC-HadRM3P_v1_mon_199001-199012.nc"  # noqa
 
 
 class WpsTestClient(WpsClient):
