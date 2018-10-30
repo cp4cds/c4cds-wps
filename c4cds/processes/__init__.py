71a3dea04d949dcb0a063805bc62443f8ea30047
Pingu Carsti <ehbrecht@dkrz.de>
2018-10-30 11:32:44 +0100

    added thumbnails

--- c4cds/processes/__init__.py
+++ c4cds/processes/__init__.py
@@ -1,9 +1,7 @@
-from .wps_sleep import Sleep
 from .wps_cmip5_regridder import CMIP5Regridder
 from .wps_cordex_subsetter import CordexSubsetter
 
 processes = [
-    Sleep(),
     CMIP5Regridder(),
     CordexSubsetter(),
 ]
